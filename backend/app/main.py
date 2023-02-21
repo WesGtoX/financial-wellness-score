5df0e9c124dc8b68a15c7d5a73378e1c462a2fcf
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2023-02-21 19:55:57 -0300

    feat(backend): adding endpoint to calculate the score

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.model import UserFinancialLife, FinancialWellnessScore
 from app.config import Settings
 
 app = FastAPI()
@@ -13,3 +14,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.post('/calculate-score/', responses={200: {}})
+async def calculate_score(user_financial_life: UserFinancialLife) -> FinancialWellnessScore:
+    return user_financial_life.result()
