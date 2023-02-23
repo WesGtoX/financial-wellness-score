890d1a480a7ff1067866242b03bf63ae34304d05
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2023-02-23 10:36:49 -0300

    feat(frontend): updating app with components and features

---
+++ frontend/src/App.styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+
+export const AppContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  flex-direction: column;
+  background-color: #f4f8fa;
+  justify-content: center;
+  align-items: center;
+`
+
+export const AppTitle = styled.span`
+  font-family: "Work Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.25rem;
+  text-align: center;
+  color: #001c95;
+  margin-bottom: 1.5rem;
+`
+
+export const AppBoldTitle = styled.span`
+  font-weight: 600;
+`
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,21 @@
+import * as S from "./App.styles"
+
+import Navbar from "./components/Navbar"
+import Card from "./components/Card"
+import Footer from "./components/Footer"
+import logo from "./assets/origin-logo.svg"
+
 const App = () => {
   return (
-    <div>Hello World!</div>
+    <S.AppContainer>
+      <Navbar logo={logo} />
+      <S.AppTitle>
+        Let's find out your{" "}
+        <S.AppBoldTitle>financial wellness score.</S.AppBoldTitle>
+      </S.AppTitle>
+      <Card />
+      <Footer />
+    </S.AppContainer>
   )
 }
 
